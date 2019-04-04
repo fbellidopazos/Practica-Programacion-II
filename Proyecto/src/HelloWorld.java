50a99ca046af92adecf075d658a277974806d13e
Fernando Bellido <fbellidopazos@gmail.com>
2019-04-04 20:55:33 +0200

    Fe
    Change-Id: I13efd7b1ccd80182ba7e6816e791c3f9ee5ded47

--- Proyecto/src/HelloWorld.java
+++ Proyecto/src/HelloWorld.java
@@ -1,15 +1,7 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
-		System.out.println("Hello World");
-		
-		for (int i = 0; i < args.length; i++) {
-			String string = args[i];
-			
-		}
-		
+		System.out.println("Test");
 		
 	}
 }
