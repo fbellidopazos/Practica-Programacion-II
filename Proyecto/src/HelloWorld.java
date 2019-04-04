b31c6b6b4d0d6e813f0f84961ceb5f3a15eb69b5
Fernando Bellido <fbellidopazos@gmail.com>
2019-04-04 20:53:37 +0200

    Test

--- Proyecto/src/HelloWorld.java
+++ Proyecto/src/HelloWorld.java
@@ -4,5 +4,12 @@ public class HelloWorld {
 		System.out.println("Hello World");
 
 		System.out.println("Hello World");
+		
+		for (int i = 0; i < args.length; i++) {
+			String string = args[i];
+			
+		}
+		
+		
 	}
 }
