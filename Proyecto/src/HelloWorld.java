8f1e3ec90eb53507e0d43a14d8d12b73b147a6e5
Fernando Bellido <fbellidopazos@gmail.com>
2019-04-04 20:54:57 +0200

    Test
    Change-Id: I1c85475e993c93579b30e9c1e85d80c00c4c9cc3

--- Proyecto/src/HelloWorld.java
+++ Proyecto/src/HelloWorld.java
@@ -4,19 +4,6 @@ public class HelloWorld {
 		System.out.println("Hello World");
 
 		System.out.println("Hello World");
-<<<<<<< HEAD
-		while (it.hasNext()) {
-			type type = (type) it.next();
-			
-		}
-=======
-		
-		for (int i = 0; i < args.length; i++) {
-			String string = args[i];
-			
-		}
-		
-		
->>>>>>> branch 'master' of https://github.com/fbellidopazos/Practica-Programacion-II
+
 	}
 }
