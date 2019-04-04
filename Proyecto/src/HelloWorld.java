1d851be4050d74a7723fa2882315f0fc9bcbac40
Fernando Bellido <fbellidopazos@gmail.com>
2019-04-04 20:45:28 +0200

    Test

--- Proyecto/src/HelloWorld.java
+++ Proyecto/src/HelloWorld.java
@@ -12,22 +12,7 @@ public class HelloWorld {
 		
 
 		
-			for (int i = 0; i < args.length; i++) {
-				String string = args[i];
-				
-			}
 			
-			
-			
-			
-			switch (key) {
-			case value:
-				
-				break;
-
-			default:
-				break;
-			}
 		}
 	
 	
