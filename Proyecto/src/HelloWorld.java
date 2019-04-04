2befa265249cb212654498633c14c90695418f96
Fernando Bellido <fbellidopazos@gmail.com>
2019-04-04 20:44:26 +0200

    Test

--- Proyecto/src/HelloWorld.java
+++ Proyecto/src/HelloWorld.java
@@ -5,36 +5,21 @@ public class HelloWorld {
 		System.err.println("Test");
 		System.out.println("testewr");
 		System.out.println("testewr");
-		
+
 		System.out.println("TEster 2");
 		while (it.hasNext()) {
 			type type = (type) it.next();
-			
+
 		}
 		for (int i = 0; i < args.length; i++) {
-		String string = args[i];
-		
-	}
-		
+			String string = args[i];
 
-		
-			for (int i = 0; i < args.length; i++) {
-				String string = args[i];
-				
-			}
-			
-			
-			
-			
-			switch (key) {
-			case value:
-				
-				break;
-
-			default:
-				break;
-			}
 		}
-	
-	
+
+		for (int i = 0; i < args.length; i++) {
+			String string = args[i];
+
+		}
+
+	}
 }
