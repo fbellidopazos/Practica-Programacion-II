186f0dcb3547dcd223c9aac35272629d30a8d532
Fernando Bellido <fbellidopazos@gmail.com>
2019-04-04 21:00:16 +0200

    Fe

--- Proyecto/src/HelloWorld.java
+++ Proyecto/src/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 		System.out.println("Hello World");
 
 		System.out.println("Hello World");
- 
+		for (int i = 0; i < args.length; i++) {
+			String string = args[i];
+			
+		}
 	}
 }
