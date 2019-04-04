8385e5cfe7f35576146bc9216a84320ff783f009
Fernando Bellido <fbellidopazos@gmail.com>
2019-04-04 20:51:05 +0200

    For LOOP

--- Proyecto/src/HelloWorld.java
+++ Proyecto/src/HelloWorld.java
@@ -2,5 +2,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		for (int i = 0; i < args.length; i++) {
+			String string = args[i];
+			
+		}
 	}
 }
