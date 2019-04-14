92878a6c4098579de3efc89920c3bf94768849b9
Fernando Bellido <fbellidopazos@gmail.com>
2019-04-14 18:42:19 +0200

    : GestionReparto
    Task-Url:

--- Proyecto/src/gestionpedidos/transportes/Furgoneta.java
+++ Proyecto/src/gestionpedidos/transportes/Furgoneta.java
@@ -1,9 +1,12 @@
 package gestionpedidos.transportes;
 
+import gestionpedidos.mapa.Mapa;
+
 public class Furgoneta extends Transporte {
 	private double tara;
 	
-	public Furgoneta (double tara) {
+	public Furgoneta (String codigo, Mapa mapa, double tara) {
+		super (codigo, mapa);
 		this.tara=tara;
 	}
 
--- Proyecto/src/gestionpedidos/transportes/FurgonetaPropia.java
+++ Proyecto/src/gestionpedidos/transportes/FurgonetaPropia.java
@@ -1,5 +1,15 @@
 package gestionpedidos.transportes;
 
-public class FurgonetaPropia {
+import gestionpedidos.mapa.Mapa;
+
+public class FurgonetaPropia extends Furgoneta {
+	private double velocidadMedia;
+	private double eurosPHora;
+	
+	public FurgonetaPropia (String codigo, Mapa mapa, double tara, double velocidadMedia, double eurosPHora) {
+		super(codigo,mapa,tara);
+		this.velocidadMedia=velocidadMedia;
+		this.eurosPHora=eurosPHoras;
+	}
 
 }
