0482f706f9d9533056168c6d18449e6a0fbb0d90
Fernando Bellido <fbellidopazos@gmail.com>
2019-05-10 16:38:19 +0200

    fr

--- Opcional_ProgramacionII/src/progii/juegotablero/model/ajedrez/piezas/Peon.java
+++ Opcional_ProgramacionII/src/progii/juegotablero/model/ajedrez/piezas/Peon.java
@@ -8,7 +8,7 @@ import progii.juegotablero.model.ajedrez.PiezaAjedrez;
 import progii.juegotablero.model.ajedrez.TipoPiezaAjedrez;
 
 public class Peon extends PiezaAjedrez {
-	private boolean start = this.getJugador().getId()==ControlJugadoresAjedrez.NEGRO?this.getFila()==6:this.getFila()==1;
+	private boolean start = this.getJugador().getId()==ControlJugadoresAjedrez.NEGRO?this.getFilaAjedrez()==7:this.getFilaAjedrez()==2;
 	
 	public Peon(Jugador jugador, int fila, char columna) {
 		super(jugador, TipoPiezaAjedrez.PEON, fila, columna);
@@ -16,12 +16,21 @@ public class Peon extends PiezaAjedrez {
 	
 	@Override
 	public ArrayList<Casilla> movimientosValidos() {
+			
 		ArrayList<Casilla> resultado = new ArrayList<>();
 		if(start) {
-			movements(resultado, 1);
-			movements(resultado, 2);
 			diagonals(resultado, 1);
 			diagonals(resultado, 2);
+			
+			if(this.getJugador().getId()==ControlJugadoresAjedrez.BLANCO && movimientoDentroLimites(this.getFila()-2, this.getColumna()) && movimientoDentroLimites(this.getFila()-1, this.getColumna()) && queHay(this.getFila()-1, this.getColumna())==null &&queHay(this.getFila()-2, this.getColumna())==null)
+				resultado.add(resultado.size(), new Casilla(this.getFila()-2, this.getColumna()));
+			else if (this.getJugador().getId()==ControlJugadoresAjedrez.NEGRO && movimientoDentroLimites(this.getFila()+2, this.getColumna())  && movimientoDentroLimites(this.getFila()+1, this.getColumna()) && queHay(this.getFila()+1, this.getColumna())==null && queHay(this.getFila()+2, this.getColumna())==null)
+				resultado.add(resultado.size(), new Casilla(this.getFila()+2, this.getColumna()));
+				
+			
+			movements(resultado, 1);
+			
+			
 			
 			start=false;
 		}
@@ -56,7 +65,7 @@ public class Peon extends PiezaAjedrez {
 	private void movements(ArrayList<Casilla> resultado,int i) {
 		if(this.getJugador().getId()==ControlJugadoresAjedrez.BLANCO && movimientoDentroLimites(this.getFila()-i, this.getColumna()) && queHay(this.getFila()-i, this.getColumna())==null)
 			resultado.add(resultado.size(), new Casilla(this.getFila()-i, this.getColumna()));
-		if (this.getJugador().getId()==ControlJugadoresAjedrez.NEGRO && movimientoDentroLimites(this.getFila()+i, this.getColumna()) && queHay(this.getFila()+i, this.getColumna())==null)
+		else if (this.getJugador().getId()==ControlJugadoresAjedrez.NEGRO && movimientoDentroLimites(this.getFila()+i, this.getColumna()) && queHay(this.getFila()+i, this.getColumna())==null)
 			resultado.add(resultado.size(), new Casilla(this.getFila()+i, this.getColumna()));
 	}	
 }
